f85fa8112c63b437e2b78298421c3c35a9a346ae
JaegerB0MB <mitchelljaeger@Mitchells-MacBook-Pro.local>
2016-02-22 10:10:56 -0600

    Initial Commit

---
+++ Assets/Scripts/StatsManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StatsManager : MonoBehaviour {
+
+	public static int lives;
+	public static int moves;
+	public static float health;
+	public static string username;
+	public static int questionIndex;
+	public static int anserIndex;
+}
